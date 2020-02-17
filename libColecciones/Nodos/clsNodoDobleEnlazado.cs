64f2a3fe959525a6ae1b994123db6ae57a841d6b
julian <jaomax.555@gmail.com>
2020-02-17 14:08:12 -0500

    Mejoras en los nodos

--- libColecciones/Nodos/clsNodoDobleEnlazado.cs
+++ libColecciones/Nodos/clsNodoDobleEnlazado.cs
@@ -4,12 +4,51 @@ namespace Servicios.Colecciones.Nodos
 {
     public class clsNodoDobleEnlazado<Tipo> : clsNodo<Tipo> where Tipo : IComparable
     {
+        #region Atributos
         private clsNodoDobleEnlazado<Tipo> atrAnterior;
         private clsNodoDobleEnlazado<Tipo> atrSiguiente;
+        #endregion
+        #region Métodos
+        #region Constructores
         public clsNodoDobleEnlazado(Tipo prmItem) : base(prmItem) { }
         public clsNodoDobleEnlazado<Tipo> darAnterior() { return atrAnterior; }
+        #endregion
+        #region Accesores - Mutadores
         public void ponerAnterior(clsNodoDobleEnlazado<Tipo> prmNodo) { atrAnterior = prmNodo; }
         public clsNodoDobleEnlazado<Tipo> darSiguiente() { return atrSiguiente; }
         public void ponerSiguiente(clsNodoDobleEnlazado<Tipo> prmNodo) { atrSiguiente = prmNodo; }
+        #endregion
+        #region Conectores y Desconectores
+        public bool ConectarSiguiente(clsNodoDobleEnlazado<Tipo> prmNodo)
+        {
+            if (prmNodo != null)
+            {
+                prmNodo.atrAnterior = this;
+                if (this.atrSiguiente != null)
+                {
+                    prmNodo.atrSiguiente = this.atrSiguiente;
+                    this.atrSiguiente.atrAnterior = prmNodo;
+                }
+                this.atrSiguiente = prmNodo;
+                return true;
+            }
+            return false;
+        }
+        public bool DesconectarSiguiente(ref Tipo prmItem)
+        {
+            clsNodoDobleEnlazado<Tipo> varNodoDesconectado;
+            if (atrSiguiente != null)
+            {
+                varNodoDesconectado = atrSiguiente;
+                atrSiguiente = atrSiguiente.atrSiguiente;
+                atrSiguiente.ponerAnterior(this);
+                prmItem = varNodoDesconectado.darItem();
+                varNodoDesconectado = null;
+                return true;
+            }
+            return false;
+        }
+        #endregion
+        #endregion
     }
 }
--- libColecciones/Nodos/clsNodoSimpleEnlazado.cs
+++ libColecciones/Nodos/clsNodoSimpleEnlazado.cs
@@ -11,9 +11,34 @@ namespace Servicios.Colecciones.Nodos
         #region Constructores
         public clsNodoSimpleEnlazado(Tipo prmItem) : base(prmItem) { }
         #endregion
-        #region Accesores Mutadores
+        #region Accesores - Mutadores
         public clsNodoSimpleEnlazado<Tipo> darSiguiente() { return atrSiguiente; }
         public void ponerSiguiente(clsNodoSimpleEnlazado<Tipo> prmNodo) { atrSiguiente = prmNodo; }
+        #endregion
+        #region Conectores y Desconectores
+        public bool ConectarSiguiente(clsNodoSimpleEnlazado<Tipo> prmNodo)
+        {
+            if(prmNodo != null)
+            {
+                prmNodo.atrSiguiente = this.atrSiguiente;
+                this.atrSiguiente = prmNodo;
+                return true;
+            }
+            return false;
+        }
+        public bool DesconectarSiguiente(ref Tipo prmItem)
+        {
+            clsNodoSimpleEnlazado<Tipo> varNodoDesconectado;
+            if (atrSiguiente != null)
+            {
+                varNodoDesconectado = atrSiguiente;
+                atrSiguiente = atrSiguiente.atrSiguiente;
+                prmItem = varNodoDesconectado.darItem();
+                varNodoDesconectado = null;
+                return true;
+            }
+            return false;
+        }
         #endregion
         #endregion
     }
